ea80bbeb9ee53756c60bbc877d29a98d27d4b276
i0ta-segfault <rudyons2005@gmail.com>
2026-05-01 21:04:10 +0530

    cors headers added

--- 7th_SemResult_React_php_mysql/backend/update_marks.php
+++ 7th_SemResult_React_php_mysql/backend/update_marks.php
@@ -1,6 +1,8 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Content-Type: application/json");
 
 $data = json_decode(file_get_contents("php://input"), true);
 
